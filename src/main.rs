6989f58cfea202bbf99a61605f47439173c8c2c4
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-24 13:34:17 +0300

    refactor: CSP policy builder usage in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web_csp::{
-    configure_csp, CspConfig, CspConfigBuilder, CspExtensions, CspMiddleware, CspPolicy,
-    CspPolicyBuilder, HashAlgorithm, HashGenerator, NonceGenerator, PolicyVerifier, Source,
+    CspPolicyBuilder, Source
 };
+use std::borrow::Cow;
 
 fn main() {
     println!("Actix Web CSP Middleware Example");
@@ -11,8 +11,9 @@ fn main() {
         .default_src([Source::Self_])
         .script_src([Source::Self_, Source::UnsafeInline])
         .style_src([Source::Self_, Source::UnsafeInline])
-        .img_src([Source::Self_, Source::Scheme("data".into())])
+        .img_src([Source::Self_, Source::Scheme(Cow::Borrowed("data"))])
         .build_unchecked();
 
     println!("Created CSP policy");
+    println!("Run examples with: cargo run --example real_world_test_fixed");
 }
